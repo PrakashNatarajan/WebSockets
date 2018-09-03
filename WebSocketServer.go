21be440698a0a4383557d0b68a5c9a872dd6d48c
PrakashNatarajan <prakashntrjn@gmail.com>
2018-09-03 15:14:01 +0530

    Modified to the chat App.

--- WebSocketServer.go
+++ WebSocketServer.go
@@ -8,31 +8,80 @@ import (
 )
 
     //var reply string
-    type socketData struct {
+    type SocketData struct {
         FrmUsr string `json:"frmusr"`
         ToUsr string `json:"tousr"`
         Msg string `json:"msg"`
     }
 
-func Echo(ws *websocket.Conn) {
+    type UserData struct {
+        UserName string `json:"user"`
+    }
+
+    var registeredClients = map[string]*websocket.Conn {}
+
+func RegisterUser(regws *websocket.Conn) {
     var err error
 
     for {
         
-        reply := &socketData{}
+        usrData := &UserData{}
 
-        if err = websocket.JSON.Receive(ws, &reply); err != nil {
-            fmt.Println("Can't receive")
+        if err = websocket.JSON.Receive(regws, &usrData); err != nil {
+            fmt.Println("Can't receive for register")
             break
         }
 
-        fmt.Println("Received back from client: " + reply.Msg)
+        registeredClients[usrData.UserName] = regws
+        
+        fmt.Println("Received back from client: " + usrData.UserName)
+        fmt.Println(usrData)
+        fmt.Println(registeredClients)
+
+        usr := "Received:  " + usrData.UserName
+        fmt.Println("Sending to client: " + usr)
+
+        if err = websocket.JSON.Send(regws, &usrData); err != nil {
+            fmt.Println("Can't send")
+            break
+        }
+    }
+}
+
+func ChatMessage(ws *websocket.Conn) {
+    var err error
+
+    for {
+        
+        reply := &SocketData{}
+
+        if err = websocket.JSON.Receive(ws, &reply); err != nil {
+            fmt.Println("Can't receive for chat")
+            break
+        }
+
+        toUserws := registeredClients[reply.ToUsr]
+
+        fmt.Println("Received back from client: " + reply.FrmUsr)
         fmt.Println(reply)
+        fmt.Println(toUserws)
 
         msg := "Received:  " + reply.Msg
         fmt.Println("Sending to client: " + msg)
 
-        if err = websocket.JSON.Send(ws, &reply); err != nil {
+        if err = websocket.JSON.Send(toUserws, &reply); err != nil {
+            fmt.Println("Can't send")
+            break
+        }
+    }
+}
+
+func HomeTest(ws *websocket.Conn) {
+    var err error
+
+    for {
+
+        if err = websocket.Message.Send(ws, "HomePageTest"); err != nil {
             fmt.Println("Can't send")
             break
         }
@@ -40,7 +89,9 @@ func Echo(ws *websocket.Conn) {
 }
 
 func main() {
-    http.Handle("/", websocket.Handler(Echo))
+    http.Handle("/chat", websocket.Handler(ChatMessage))
+    http.Handle("/reg", websocket.Handler(RegisterUser))
+    http.Handle("/", websocket.Handler(HomeTest))
     fmt.Println("Started the WebSocketServer with port no: 1234")
     fmt.Println("Use CTRL + C to shutdown")
     if err := http.ListenAndServe(":1234", nil); err != nil {
