67287234f2f269ddc51dace7ee456cf3e8d0456b
PrakashNatarajan <prakashntrjn@gmail.com>
2018-09-10 16:43:42 +0530

    Initial commit.

---
+++ README.md
@@ -0,0 +1,16 @@
+# Source 
+https://www.thepolyglotdeveloper.com/2016/12/create-real-time-chat-app-golang-angular-2-websockets/
+<br />
+# WebSockets
+Command To Start the WebSocketServer
+<br />
+go run ChatServer.go
+<br />
+or
+<br />
+go run clientmanager.go client.go main.go
+<br />
+Command To Shotdown the WebSocketServer
+<br />
+CTRL + C
+<br />
---
+++ clientmanager.go
@@ -0,0 +1,62 @@
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+    "encoding/json"
+)
+
+// ClientManager maintains the set of active clients and broadcasts messages to the
+// clients.
+type ClientManager struct {
+	// Registered clients.
+	clients map[*Client]bool
+
+	// Inbound messages from the clients.
+	broadcast chan []byte
+
+	// Register requests from the clients.
+	register chan *Client
+
+	// Unregister requests from clients.
+	unregister chan *Client
+}
+
+func newClientManager() *ClientManager {
+	return &ClientManager{
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		clients:    make(map[*Client]bool),
+	}
+}
+
+
+func (manager *ClientManager) start() {
+    for {
+        select {
+        case conn := <-manager.register:
+            manager.clients[conn] = true
+            jsonMessage, _ := json.Marshal(&Message{Content: "/A new socket has connected."})
+            manager.send(jsonMessage, conn)
+        case conn := <-manager.unregister:
+            if _, ok := manager.clients[conn]; ok {
+                close(conn.send)
+                delete(manager.clients, conn)
+                jsonMessage, _ := json.Marshal(&Message{Content: "/A socket has disconnected."})
+                manager.send(jsonMessage, conn)
+            }
+        case message := <-manager.broadcast:
+            for conn := range manager.clients {
+                select {
+                case conn.send <- message:
+                default:
+                    close(conn.send)
+                    delete(manager.clients, conn)
+                }
+            }
+        }
+    }
+}
